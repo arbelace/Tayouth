f7ba55b6dac1c0958007ecc0da8a7b0eb2c9058a
arbelace <130295772+arbelace@users.noreply.github.com>
2024-01-25 21:53:00 +0800

    UI
    Binago ko na yung UI

--- Tayouth/index.php
+++ Tayouth/index.php
@@ -4,23 +4,45 @@
     include('functions/userfunction.php');
     include('./includes/header.php');
     ?>
-    <section>
-        <video autoplay muted loop id="myVideo" class="bg-vid" style="background-image: linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7));">
+    <section> 
+    <video autoplay muted loop id="myVideo" class="bg-vid">
             <source src="img/videohome.mp4" type="video/mp4">
         </video>
         <div class="container">
             <div class="row min-vh-100 align-items-center">
-                <div class="col-lg-12 col-sm-12">
-                    <h1 style="color:#e9aa01; letter-spacing: 0px; font-size: 20px;">WELCOME TO TAYOUTH<h1>
-                            <h1 style="color:white; letter-spacing: 0px; font-size: 60px;"> Let's Save The World<br>By Savings Lives</h1>
+                <div class="col-lg-12 col-sm-12 text-left">
+                    <h2 style="color:#e9aa01; letter-spacing: 0px; font-size: 20px;">WELCOME TO TAYOUTH<h2>
+                            <h2 style="color:white; letter-spacing: 0px; font-size: 60px;"> Let's Save The World<br>By Savings Lives</h2>
                             <Volunteer style="color:white;">Every heartbeat matters. Every breath deserves a<br>chance. Every single step we take together brings us<br>closer to a world where no life is left behind.</p>
+                                <!-- <h3  style="letter-spacing: 0px; font-size: 15px;" class="pb-lg-0 text-white"></h3> -->
                                 <button style="color:white;" type="button" class="btn btn-outline-warning">Join Us! <span style='font-size:18x;'>&#8599;</span></button>
-                                <!-- <p id="text" class="text-white ps-lg-4"></p> -->
                 </div>
             </div>
         </div>
     </section>
     <section>
+    <h1 style="color: #070366;" class="mb-lg-5 mb-sm-3">Lorem ipsum dolor.</h1>
+    <div class="container">
+        <div class="row">
+            <div class="course-col">
+                <h3 style="color: #070366;">Lorem ipsum dolor sit</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="course-col">
+                <h3 style="color: #070366;">Lorem ipsum dolor sit</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div class="course-col">
+                <h3 style="color: #070366;">Lorem ipsum dolor sit</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua..</p>
+            </div>
+        </div>
+    </div>
+</section>
+    <!-- <section>
         <div class="container">
             <div class="row align-items-center">
                 <div class="col-4">
@@ -40,21 +62,133 @@
                 </div>
             </div>
         </div>
+    </section> -->
+    <section style="background-image: linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(img/ewan1.png )
+     !important; background-repeat: no-repeat; background-position: left !important; background-size: cover; background-attachment: fixed;">
+        <div class="container">
+            <div class="container">
+                <div class="row min-vh-50 align-items-center ">
+                    <div class="col-lg-6 col-sm-12">
+                        <img src="img/gege.png" data-aos="zoom-in" style=" width:100%; border-radius: 5px;">
+                    </div>
+                    <div class="col-lg-6 col-sm-12 text-right text-black ps-lg-5">
+                        <h3 style="color:#e9aa01;" class="text-left"><span>What Is TaYouth?</span></u></h3>
+                        <p style="text-align: justify; color:#e9aa01;">TaYouth is a community group formed by a youth sector founded by Michael John Serna, a former Angat Buhay Literacy Hub Barangay XI operation head. TaYouth is a dynamic volunteer group dedicated to serving and uplifting communities, with a special focus on children. Its mission is to inspire positive change, foster holistic development, and create a lasting impact on the lives of the youth. TaYouth’s vision is to reach a wider community which results in the conducting “Barangay Arangkada Goal.”</p>
+                        <h3 style="color:#e9aa01;" class="text-left"><span>About Us</span></u></h3>
+                        <p style="text-align: justify; color:#e9aa01;"> Date founded Tayouth was founded last October 1, 2022 when Angat Buhay Literacy Hub Barangay XI Lucena City participated in TAYO Awards dedicated for 10 outstanding projects in the Philippines.</p>
+                        <button style="color: #e9aa01;" type="button" class="btn btn-sm-secondary mb-5 ">View More! <i class="fa-solid fa-arrow-right"></i></button>
+
+                    </div>
+                </div>
+            </div>
     </section>
-    <section style="padding-bottom: 100px; background-color: #e4e4e4;">
+    <section>
+        <div class="container mb-lg-4">
+            <h1 style="color: #070366;" class="pt-lg-4 text-left mb-1">We Are TaYouth</h1>
+            <p style="color: #070366;" class="text-center mb-1">Organization set up to provide help and raise money for those in need</p>
+            <div class="row">
+                <div class="facilities-col">
+                    <img src="img/1.png" data-aos="zoom-in">
+                    <h3 style="color: #070366;" class="text-start">Mission</h3>
+                    <p style="color: #070366;" class="text-left">The organization shall pursue empowering the next generation to thrive by providing a nurturing and inclusive community that fosters personal growth, leadership development, and social responsibility. We strive to inspire young individuals to realize their full potential, cultivate resilience, and become active, compassionate contributors to a better world.</p>
+                </div>
+                <div class="facilities-col">
+                    <img src="img/2.png" data-aos="zoom-in">
+                    <h3 style="color: #070366;" class="text-left">Vision</h3>
+                    <p style="color: #070366;" class="text-left">The organization shall envision the future where every young person has the knowledge, skills, and opportunities to shape their own success and positively impact their communities. We aspire to be a driving force in creating a world where youth are empowered to lead, innovate, and build a brighter tomorrow.</p>
+                </div>
+                <div class="facilities-col">
+                    <img src="img/3.png" data-aos="zoom-in">
+                    <h3 style="color: #070366;" class="text-left;">Core Values</h3>
+                    <p style="color: #070366;" class="text-left">The core values of the Tayouth typically include collaboration, compassion, integrity, inclusivity, and dedication to community service.</p>
+                </div>
+            </div>
+    </section>
+    <section>
+    <div id="hero-carousel" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-indicators">
+            <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#hero-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+
+        <div class="carousel-inner">
+            <div class="carousel-item active c-item">
+                <section class=c-img style="background-image: linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(img/ewan1.png)
+     !important; background-repeat: no-repeat; background-position: left !important; background-size: cover; background-attachment: fixed;">
+                    <div class="carousel-caption top-0 mt-4">
+                        <p class="text-white mt-5 fs-3 text-uppercase">News & Stories</p>
+                        <h1 class="text-white display-1 fw-bolder text-capitalize">Lorem</h1>
+                        <button type="button" class="btn btn-outline-light">Learn More</button>
+                    </div>
+            </div>
+            <div class="carousel-item c-item">
+                <section class=c-img style="background-image: linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(img/ewan1.png)
+     !important; background-repeat: no-repeat; background-position: left !important; background-size: cover; background-attachment: fixed;">
+                    <div class="carousel-caption top-0 mt-4">
+                        <p class="text-white text-uppercase fs-3 mt-5">News & Stories</p>
+                        <p class="text-white display-1 fw-bolder text-capitalize">Lorem</p>
+                        <button type="button" class="btn btn-outline-light">Learn More</button>
+                    </div>
+            </div>
+            <div class="carousel-item c-item">
+                <section class=c-img style="background-image: linear-gradient(rgba(4, 9, 30, 0.7), rgba(4, 9, 30, 0.7)), url(img/ewan1.png)
+     !important; background-repeat: no-repeat; background-position: left !important; background-size: cover; background-attachment: fixed;">
+                    <div class="carousel-caption top-0 mt-4">
+                        <p class="text-white text-uppercase fs-3 mt-5">News & Stories</p>
+                        <p class="text-white display-1 fw-bolder text-capitalize">Lorem</p>
+                        <button type="button" class="btn btn-outline-light">Learn More</button>
+                    </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#hero-carousel" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#hero-carousel" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</section>
+    <section>
+        <div class="container">
+            <h1 style="color: #070366;" class="pt-lg-4 text-left mb-1">How You Can Help?</h1>
+            <p style="color: #070366;" class="text-center mb-1">lorem lorem</p>
+            <div class="row">
+                <div class="facilities-col">
+                    <img src="img/are.png" data-aos="zoom-in">
+                    <h3 style="color: #070366;" class="text-left">Be a fundraiser</h3>
+                    <p style="color: #070366;" class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </p><button style="color:#070366;" type="button" class="btn btn-outline">Learn More <span style='font-size:18x;'>&#8599;</span></button>
+                </div>
+                <div class="facilities-col">
+                    <img src="img/them.png" data-aos="zoom-in">
+                    <h3 style="color: #070366;" class="text-left">Attend an event</h3>
+                    <p style="color: #070366;" class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </p><button style="color:#070366;" type="button" class="btn btn-outline">Learn More <span style='font-size:18x;'>&#8599;</span></button>
+                </div>
+                <div class="facilities-col">
+                    <img src="img/we.png" data-aos="zoom-in">
+                    <h3 style="color: #070366;" class="text-left;">Becoming a donor</h3>
+                    <p style="color: #070366;" class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </p><button style="color:#070366;" type="button" class="btn btn-outline">Learn More <span style='font-size:18x;'>&#8599;</span></button>
+                </div>
+            </div>
+    </section>
+    <section style="padding-bottom: 100px; background-color:white;">
         <div class="container">
             <div class="row align-items-center">
                 <div class="row text-center mt-5 mb-lg-5">
-                    <h1 style="letter-spacing: 5px; font-size: 20pt;" class="pb-lg-0 mb-lg-0">How It Works</h1>
-                    <p style="font-size: small;">Organization set up to provide help and raise money for those in need</p>
+                    <h1 style="color: #070366;" >How It Works</h1>
+                    <p style="color: #070366;" >Organization set up to provide help and raise money for those in need</p>
                 </div>
-
                 <div class="col-lg-4">
                     <div class="card text-center card_home">
                         <i class="fa-solid fa-gift h1 mt-lg-3" style="color:#D75E0B;"></i>
                         <div class="card-body ">
-                            <p class="card-text">People like you give to your favorite projects; you feel great when you get updates about how your money is put to work by trusted organizations.</p>
-                            <h5 class="card-title">DONORS</h5>
+                            <p style="color: #070366;" >People like you give to your favorite projects; you feel great when you get updates about how your money is put to work by trusted organizations.</p>
+                            </p><button style="font-size:27px; color:#070366;" type="button" class="btn btn-outline">DONORS<span></span></button>
                         </div>
                     </div>
                 </div>
@@ -62,8 +196,8 @@
                     <div class="card text-center card_home">
                         <i class="fa-solid fa-building h1 mt-lg-3" style="color:#0B5ED7;"></i>
                         <div class="card-body">
-                            <p class="card-text">Generous companies and their employees further support high-impact projects with donations and grants, helping local communities thrive.</p>
-                            <h5 class="card-title">COMPANIES</h5>
+                            <p style="color: #070366;" >Generous companies and their employees further support high-impact projects with donations and grants, helping local communities thrive.</p>
+                            </p><button style="font-size:27px; color:#070366;" type="button" class="btn btn-outline">COMPANIES<span></span></button>
                         </div>
                     </div>
                 </div>
@@ -71,8 +205,8 @@
                     <div class="card text-center card_home">
                         <i class="fa-solid fa-seedling h1 mt-lg-3" style="color:#FEDC25;"></i>
                         <div class="card-body">
-                            <p class="card-text">Nonprofits have the funding they need to listen to feedback and try out new ways to work; communities all over the globe get more awesome!</p>
-                            <h5 class="card-title">OUR IMPACT</h5>
+                            <p style="color: #070366;" >Nonprofits have the funding they need to listen to feedback and try out new ways to work; communities all over the globe get more awesome!</p>
+                            </p><button style="font-size:27px; color:#070366;" type="button" class="btn btn-outline">OUR IMPACT <span></span></button>
                         </div>
                     </div>
                 </div>
@@ -81,12 +215,12 @@
         </div>
     </section>
 
-    <section style="padding-bottom: 100px; background-color: #e4e4e4;">
+    <section style="padding-bottom: 100px; background-color:var(--section);">
         <div class="container">
             <div class="row align-items-center">
                 <div class="row text-center mt-5 mb-5">
-                    <h1 style="letter-spacing: 5px; font-size: 20pt; color: #1f8555;" class="pb-lg-0 mb-lg-0">Customer Service</h1>
-                    <p style="font-size: small;">We always welcome your feedback about our service and your in-store experience – whether you found it friendly and helpful, or it fell short of your expectations.</p>
+                    <h1 style="color:var(--first-color);" class="pb-lg-0 mb-lg-0">Feedback</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
                 </div>
 
                 <div class="col-lg-6">
@@ -108,7 +242,7 @@
                             <textarea cols="30" rows="4" class="form-control"></textarea>
                         </div>
 
-                        <button type="button" class="btn btn-outline-success btn-sm">Submit</button>
+                        <button type="button" class="btn btn-outline-primary btn-sm">Submit</button>
 
                     </form>
                 </div>
